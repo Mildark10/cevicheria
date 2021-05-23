95ec1f224276d027d67f5df00451752d321a08ef
Milton <sonix-141@hotmail.com>
2021-05-22 20:54:12 -0500

    csss

--- javascript/js.js
+++ javascript/js.js
@@ -1,3 +1,14 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 1900);
+}
+
+function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
+
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     header.classList.toggle("sticky", window.scrollY > 0);
